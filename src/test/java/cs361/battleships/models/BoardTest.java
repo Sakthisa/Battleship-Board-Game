145891be6b43357e52d55eebb6a7207fe98ab51e
Sakthisa <sakthisa@oregonstate.edu>
2018-10-02 20:35:24 -0700

    Test cases for placing ships

--- src/test/java/cs361/battleships/models/BoardTest.java
+++ src/test/java/cs361/battleships/models/BoardTest.java
@@ -8,15 +8,46 @@ import static org.junit.Assert.assertTrue;
 
 public class BoardTest {
 
+
+    //Checking invalid Minesweeper placement
     @Test
-    public void testInvalidPlacement() {
+    public void testInvalidPlacementMineSweeper() {
         Board board = new Board();
-        assertFalse(board.placeShip(new Ship("MINESWEEPER"), 11, 'C', true));
+        assertFalse(board.placeShip(new Ship("MINESWEEPER"), 11, 'C', false));
     }
 
+    //Checking valid Destroyer placement
     @Test
-    public void testValidPlaceShip(){
+    public void testValidPlaceShipMineSweeper(){
         Board board = new Board();
         assertTrue(board.placeShip(new Ship("MINESWEEPER"), 5, 'C', false));
+    }
+
+    //Checking invalid Destroyer placement
+    @Test
+    public void testInvalidPlaceShipDestroyer(){
+        Board board = new Board();
+        assertFalse(board.placeShip(new Ship("DESTROYER"), 10, 'I', false));
+    }
+
+    //Checking valid Destroyer placement
+    @Test
+    public void testValidPlaceShipDestroyer(){
+        Board board = new Board();
+        assertTrue(board.placeShip(new Ship("DESTROYER"), 3, 'A', true));
+    }
+
+    //Checking invalid Battleship placement
+    @Test
+    public void testInvalidPlaceShipBattleship(){
+        Board board = new Board();
+        assertFalse(board.placeShip(new Ship("BATTLESHIP"), 8, 'H', true));
+    }
+
+    //Checking valid Battleship placement
+    @Test
+    public void testValidPlaceShipBattleship(){
+        Board board = new Board();
+        assertTrue(board.placeShip(new Ship("BATTLESHIP"), 10, 'G', false));
     }
 }
