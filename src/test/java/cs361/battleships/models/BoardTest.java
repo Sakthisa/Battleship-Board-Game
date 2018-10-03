b83c0d01adb0235ba68fd32924629a535c285f5f
Sakthisa <sakthisa@oregonstate.edu>
2018-10-02 20:04:34 -0700

    Added test case for placeship

--- src/test/java/cs361/battleships/models/BoardTest.java
+++ src/test/java/cs361/battleships/models/BoardTest.java
@@ -2,7 +2,9 @@ package cs361.battleships.models;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class BoardTest {
 
@@ -10,5 +12,11 @@ public class BoardTest {
     public void testInvalidPlacement() {
         Board board = new Board();
         assertFalse(board.placeShip(new Ship("MINESWEEPER"), 11, 'C', true));
+    }
+
+    @Test
+    public void testValidPlaceShip(){
+        Board board = new Board();
+        assertTrue(board.placeShip(new Ship("MINESWEEPER"), 5, 'C', false));
     }
 }
