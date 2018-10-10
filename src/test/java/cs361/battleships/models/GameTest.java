96d5b6bc05438e1a2f587eeb51d6a1265d5fdfc1
Sakthisa <sakthisa@oregonstate.edu>
2018-10-10 14:47:29 -0700

    Has to be a valid ship name

--- src/test/java/cs361/battleships/models/GameTest.java
+++ src/test/java/cs361/battleships/models/GameTest.java
@@ -14,7 +14,7 @@ public class GameTest { // This class has 100% coverage of all lines in Game.jav
         Game game = new Game(); // Creates new game
         assertTrue(game.placeShip(new Ship("MINESWEEPER"), 1, 'A', true)); // places minesweeper ship at 1A vertically
         assertTrue(game.placeShip(new Ship("DESTROYER"), 1, 'C', true)); // places destroyer ship at 1A vertically (This test currently fails)
-        assertTrue(game.placeShip(new Ship("xyz"), 1, 'F', true)); // places other type ship at 1A vertically
+        assertFalse(game.placeShip(new Ship("xyz"), 1, 'F', true)); // not valid ship name
         assertFalse(game.placeShip(new Ship("MINESWEEPER"), 1, 'J', true)); // places 4th ship on board (not possible)
         assertFalse(game.placeShip(new Ship("MINESWEEPER"), 1, 'A', true)); // places ship in already chosen location (not possible)
         Game gamebp = new Game();
@@ -23,7 +23,7 @@ public class GameTest { // This class has 100% coverage of all lines in Game.jav
         Game game2 = new Game();
         assertTrue(game2.placeShip(new Ship("MINESWEEPER"), 1, 'A', false)); // places minesweeper ship at 1A horizontally
         assertTrue(game2.placeShip(new Ship("DESTROYER"), 9, 'A', false)); // places destroyer ship at 1A horizontally
-        assertTrue(game2.placeShip(new Ship("xyz"), 8, 'A', false)); // places other type ship at 1A vertically
+        assertFalse(game2.placeShip(new Ship("xyz"), 8, 'A', false)); // not valid ship name
         Game gamebph = new Game();
         assertFalse(gamebph.placeShip(new Ship("MINESWEEPER"), 0, 'A', false)); // places minesweeper ship at 0A horizontally (not possible)
         assertFalse(gamebph.placeShip(new Ship("MINESWEEPER"), 1, 'Z', false)); // places minesweeper ship at 1Z horizontally (not possible)
@@ -39,4 +39,5 @@ public class GameTest { // This class has 100% coverage of all lines in Game.jav
         assertFalse(game.attack(1, 'a')); // attacks at 1a (with lowercase so not possible)
         assertFalse(game.attack(-1, 'A')); // attacks at -1A (not possible to have negative coordinates)
     }
+
 }
