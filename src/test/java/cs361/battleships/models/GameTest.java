58201db639cd834721c5038707f60209222cc742
Gray Lunn <lunng@oregonstate.edu>
2018-10-10 06:49:25 -0700

    Added game test class

---
+++ src/test/java/cs361/battleships/models/GameTest.java
@@ -0,0 +1,17 @@
+package cs361.battleships.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class GameTest {
+
+    @Test
+    public void testInvalidShipPlacement(){
+        Ship ship = new Ship("BATTLESHIP");
+        
+    }
+
+}
