75e7b59559f9f8009dfe5f34a1be4e0f705f5aaf
Gray Lunn <lunng@oregonstate.edu>
2018-10-24 21:47:24 -0700

    Changed link location for information, changed function of how to play

--- src/main/java/assets/game.js
+++ src/main/java/assets/game.js
@@ -182,19 +182,26 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+var span2 = document.getElementsByClassName("close")[1];
+
 // When the user clicks the button, open the modal
 btn.onclick = function() {
+    modal = document.getElementById('myModal');
     modal.style.display = "block";
 }
 
 
-//TODO: change this to toggle a different window
 function displayVictoryDialogue(){
+    modal = document.getElementById('endModal');
     modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+    modal.style.display = "none";
+}
+
+span2.onclick = function() {
     modal.style.display = "none";
 }
 
