a7413cfa3719f118f3c56120ce6d9c97194d0166
ShaneClancy <clancys@oregonstate.edu>
2018-11-06 12:08:39 -0800

    Added initialization test for Minesweeper

--- src/test/java/cs361/battleships/models/MinesweeperTest.java
+++ src/test/java/cs361/battleships/models/MinesweeperTest.java
@@ -1,4 +1,17 @@
 package cs361.battleships.models;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class MinesweeperTest {
+
+    @Test
+    public void testBattleshipCreation(){
+        Minesweeper m = new Minesweeper();
+        assertEquals(2, m.getShipSize());
+        assertEquals("MINESWEEPER", m.getKind());
+    }
 }
