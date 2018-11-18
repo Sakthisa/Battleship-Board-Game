a4f904d236d96562e52cd6e83cebc7268e963652
Sakthisa <sakthisa@oregonstate.edu>
2018-11-17 16:13:01 -0800

    Non-submerge Submarine frontend/backend logic

--- src/main/java/cs361/battleships/models/Submarine.java
+++ src/main/java/cs361/battleships/models/Submarine.java
@@ -1,7 +1,6 @@
 package cs361.battleships.models;
 
 public class Submarine extends Ship{
-
     public Submarine() {
         this.shipSize = 5;
         this.kind = "SUBMARINE";
---
+++ src/test/java/cs361/battleships/models/SubmarineTest.java
@@ -0,0 +1,17 @@
+package cs361.battleships.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SubmarineTest {
+
+    @Test
+    public void testSubmarineCreation(){
+        Submarine s = new Submarine();
+        assertEquals(5, s.getShipSize());
+        assertEquals("SUBMARINE", s.getKind());
+    }
+}
